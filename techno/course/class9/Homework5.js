aca6cb4e14725f0234c03ed1c0cf3aecd10b3c4b
Janery <janery@gmail.com>
2022-10-19 03:41:23 -0400

    homework5

---
+++ techno/course/class9/Homework5.js
@@ -0,0 +1,53 @@
+// Due date : Oct-19 EOD
+
+/**
+ * Q1. Find the average of the given numbers-array
+ * 
+ * [1,2,3,4,5] -> 15/5 -> 3
+ * [1, -1, 2, -3, 1] -> 0/5 -> 0
+ * [1, 2, 1, 1, 3, 1, 2] -> 11/7 -> 1.xxx
+ * 
+ */
+let numbers = [23, 12, 43, 10, 5, -10];
+let num1 = 23
+let num2 = 12
+let num3 = 43
+let num4 = 10
+let num5 = 5
+let num6 = -10
+
+let total = 83
+console.log(`${num1} + ${num2} + ${num3} + ${num4} + ${num5} + ${num6} = ${total}`);
+
+console.log(`let average of array = ${total/6}`);
+
+
+/**
+ * Q2. Reverse a given string (word by word)
+ * 
+ * 'have a great day' -> day great a have
+ * 'good morning' -> morning good
+ * 'Learn' -> Learn
+ * 
+ * 
+ */
+let str2 = 'have a great day'   // day great a have
+let reverseStr2 = 'day great a have';
+// code
+console.log(`have a great day -> reverseStr2`)
+console.log(`reverseStr2 -> ${reverseStr2}`);   // day great a have
+
+
+/**
+ * Q3. Create abbreviation for a sentence
+ * 
+ * 'you live Only once' -> YLOO
+ * 'you Never waLK ALONe every day' -> YNWAED
+ * 'good Morning' -> 'GM'
+ * 'hello dear, how are you doing?' -> HDHAYD
+ */
+let str3 = 'good Morning';
+let abbr = 'GM';
+// code
+console.log(`good Morning -> abbr ${abbr}`)
+console.log(`abbr -> ${abbr}`);
