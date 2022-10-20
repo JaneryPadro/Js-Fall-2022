626fc2b5c1ca062389011accfcb917a488643e19
Janery <janery@gmail.com>
2022-10-20 10:11:04 -0400

    Homework6

---
+++ techno/course/class10/Homework6.js
@@ -0,0 +1,170 @@
+//  Due: Fri (Oct 21)
+
+
+/**
+ * Q1. Print the object data as mentioned above.
+ */
+ let student1 = {
+    name : 'John Doe',
+    state : 'NJ',
+    age : 20,
+    gender : 'M',
+    paymentMethod : 'finance',
+    anyExperience : true,
+    pastJobs : [
+        {
+            companyName : 'ABC',
+            numOfYears : 2,
+            title : 'DEF'
+        },
+        {
+            companyName : 'EFG',
+            numOfYears : 3,
+            title : 'HIJ'
+        },
+        {
+            companyName : 'KLM',
+            numOfYears : 4,
+            title : 'NOP'
+        }
+    ],
+    knownSkills: ['Java', 'Manual testing', 'Js basics', 'Programming', 'CS basics']
+}
+//code for Q1
+
+for (const attr in student1) {
+    console.log(attr);
+    console.log(student1[attr]);
+}
+
+/*
+    name : John Doe
+    state : NJ
+    age : 20
+    gender : M
+    paymentMethod : finance
+    anyExperience : true
+    pastJobs : Yes (3)
+        companyName : ABC
+        numOfYears : 2
+        title : 'DEF'
+
+        companyName : EFG
+        numOfYears : 3
+        title : 'HIJ'
+        
+        companyName : KLM
+        numOfYears : 4
+        title : 'NOP'
+    knownSkills : 5 (Java, Manual testing, Js basics, Programming, CS basics)
+*/
+
+
+let student1_2 = {
+    name : 'Eli hen',
+    state : 'NY',
+    age : 22,
+    gender : 'F',
+    paymentMethod : 'Paid',
+    anyExperience : false,
+    pastJobs : 'No',
+    knownSkills: ['Java', 'Manual testing', 'CS basics']
+}
+/*
+    name : Eli hen
+    state : NY
+    age : 22
+    gender : F
+    paymentMethod : Paid
+    anyExperience : false
+    pastJobs : No
+    knownSkills : 3 (Java, Manual testing, CS basics)
+*/
+//code for Q1
+console.log(`\n\nPrinting Objects Data`);
+for (const attr in student1_2) {
+    console.log(attr);
+    console.log(student1_2[attr]);
+}
+
+
+
+/**
+ * Q2: Print the index for SECOND occurrence of 'baseBalL' (Ignoring cases)
+ * 
+ * if BaseBall is NOT present in Array: print, Baseball is not present in the given array
+ * 
+ * if BaseBall is present Only once in Array: print, Baseball is present only once in the given array
+ * 
+ * if BaseBall is present two or more times in Array: print, Baseball has second occurrence at index-X
+ *  
+ */
+let favSports_1 = ['Football', 'Soccer', 'Fishing', 'BASKETBALL', 'Ping pong', 'Swimming', 'Running'];
+
+let favSports_2 = ['Football', 'Soccer', 'BaseBall', 'Ping pong', 'Rugby', 'BASKETBALL', 'Ping pong', 'Swimming', 'Running'];
+
+let favSports_3 = ['Football', 'Soccer', 'Baseball', 'Fishing', 'Baseball', 'Ping pong', 'Baseball', 'Rugby', 'BASKETBALL', 'Ping pong', 'Swimming', 'Running'];
+
+
+/**
+ * Q3. Print the total number of past experience for a student
+ * 
+ * Assumption:
+ * In studentObject, anyExperience-attribute will be true, if has any past-experience, 
+ * otherwise anyExperience-attribute will be false
+ * 
+ * If student has any past experience, print :
+ *      studentName has total X-years of experience
+ * 
+  * If student has NO past experience, print :
+ *      studentName has no past experience
+ */
+let student3_1 = {
+    name : 'John Doe',
+    state : 'NJ',
+    age : 20,
+    gender : 'M',
+    paymentMethod : 'finance',
+    anyExperience : true,
+    pastJobs : [
+        {
+            companyName : 'ABC',
+            numOfYears : 2,
+            title : 'DEF'
+        },
+        {
+            companyName : 'EFG',
+            numOfYears : 3,
+            title : 'HIJ'
+        },
+        {
+            companyName : 'KLM',
+            numOfYears : 4,
+            title : 'NOP'
+        }
+    ],
+    knownSkills: ['Java', 'Manual testing', 'Js basics', 'Programming', 'CS basics']
+}
+//code for Q3
+
+console.log(`\n\nStudent has 9 years of experience\n\n`);
+for (const attr in student3_1) {
+    console.log(student3_1.anyExperience);
+    console.log(student3_1.pastJobs[0].companyName);
+}
+
+let student3_2 = {
+    name : 'Eli hen',
+    state : 'NY',
+    age : 22,
+    gender : 'F',
+    paymentMethod : 'Paid',
+    anyExperience : false,
+    pastJobs : [],
+    knownSkills: ['Java', 'Manual testing', 'CS basics']
+}
+// code for Q3
+for (const attr in student3_2) {
+    console.log(attr);
+    console.log(student3_2[attr]);
+}
